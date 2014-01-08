74c665d4e3725434727e168e2cc7c3041a54228f
Kagan Riedel <kaganriedel@gmail.com>
2014-01-08 13:14:53 -0600

    Initial commit

---
+++ Adventure/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Adventure
+//
+//  Created by Kagan Riedel on 1/8/14.
+//  Copyright (c) 2014 Kagan Riedel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
